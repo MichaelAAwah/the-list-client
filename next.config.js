b2d397b39e6c8b7a484c3372b457111961012b72
MichaelAAwah <awahmichael441@gmail.com>
2024-12-06 16:10:48 +0000

    Updated next config base

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/the-list-client/', // Replace with your GitHub repository name
+  assetPrefix: '/the-list-client/',
   eslint: {
     ignoreDuringBuilds: true,
   },
