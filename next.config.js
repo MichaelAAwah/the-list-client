0932eda81d08dbb05df1abf7bf62fa4d95e908b4
MichaelAAwah <awahmichael441@gmail.com>
2024-12-06 16:12:14 +0000

    minor fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/the-list-client/', // Replace with your GitHub repository name
+  basePath: '/the-list-client', // Replace with your GitHub repository name
   assetPrefix: '/the-list-client/',
   eslint: {
     ignoreDuringBuilds: true,
