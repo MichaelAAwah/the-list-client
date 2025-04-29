2824e5e53f7c0502188e233e2f1a326ba7d1debc
MichaelAAwah <awahmichael441@gmail.com>
2025-04-29 15:35:31 +0000

    minor fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   output: 'export',
   basePath: '/the-list-client', // Replace with your GitHub repository name
   assetPrefix: '/the-list-client/',
+  trailingSlash: true, // this helps with static export routing
   eslint: {
     ignoreDuringBuilds: true,
   },
